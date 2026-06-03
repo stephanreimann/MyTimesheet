e1ee780de77f5eed0fcac05ed842b387f27d4ee7
Stephan Reimann <stephan.reimann@siemens.com>
2026-06-03 12:00:19 +0200

    Add missing FXML elements

--- src/controller/XmlEditorViewController.java
+++ src/controller/XmlEditorViewController.java
@@ -4,36 +4,31 @@
  */
 package controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.ResourceBundle;
+import java.util.regex.*;
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.*;
 import net.fortuna.ical4j.data.ParserException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import service.LanguageService;
-import service.UndoService;
+import org.apache.logging.log4j.*;
+import org.fxmisc.richtext.*;
+import org.fxmisc.richtext.model.*;
+import org.xml.sax.*;
+import service.*;
 import utils.EventManager;
 
 /**
@@ -42,6 +37,19 @@ import utils.EventManager;
  */
 public class XmlEditorViewController implements Initializable, IViewController {
 
+    private static final Pattern XML_TAG = Pattern.compile("(?<ELEMENT>(</?\\h*)(\\w+)([^<>]*)(\\h*/?>))"
+    		+"|(?<COMMENT><!--(.|\\v)+?-->)");
+    
+    private static final Pattern ATTRIBUTES = Pattern.compile("(\\w+\\h*)(=)(\\h*\"[^\"]+\")");
+    
+    private static final int GROUP_OPEN_BRACKET = 2;
+    private static final int GROUP_ELEMENT_NAME = 3;
+    private static final int GROUP_ATTRIBUTES_SECTION = 4;
+    private static final int GROUP_CLOSE_BRACKET = 5;
+    private static final int GROUP_ATTRIBUTE_NAME = 1;
+    private static final int GROUP_EQUAL_SYMBOL = 2;
+    private static final int GROUP_ATTRIBUTE_VALUE = 3;
+    
     private final String selectResourceKey = "Select";
     private final String acceptResourceKey = "Accept";
     private final String cancelResourceKey = "Cancel";
@@ -50,15 +58,29 @@ public class XmlEditorViewController implements Initializable, IViewController {
     private final Logger log = LogManager.getLogger(XmlEditorViewController.class.getName());
 
     @FXML
+    AnchorPane anchorPane;
+    
+    @FXML
+    VBox vBox;
+    
+    @FXML
+    HBox hBox;
+                       
+    @FXML
     TextField filePathAndNameTextFieldValue;
+    
     @FXML 
     Button selectButton;
 
     @FXML
-    TextArea xmlTextArea;
+    CodeArea codeArea;
     
+    @FXML
+    ToolBar toolBar;
+            
     @FXML 
     private Button acceptButton;
+    
     @FXML
     private Button cancelButton;
 
@@ -108,15 +130,62 @@ public class XmlEditorViewController implements Initializable, IViewController {
     }
     
     private void loadFromFile(String filePathAndName) throws IOException, ParserException {
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+        
         String content = Files.readString(Path.of(filePathAndName), StandardCharsets.UTF_8);
-        xmlTextArea.setText(content);
+        codeArea.replaceText(content);
         originalText = content;
+        codeArea.setStyleSpans(0, computeHighlighting(content));
     }
-     
+
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {	
+        Matcher matcher = XML_TAG.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+        while(matcher.find()) {
+        	
+        	spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+        	if(matcher.group("COMMENT") != null) {
+        		spansBuilder.add(Collections.singleton("comment"), matcher.end() - matcher.start());
+        	}
+        	else {
+        		if(matcher.group("ELEMENT") != null) {
+        			String attributesText = matcher.group(GROUP_ATTRIBUTES_SECTION);
+        			
+        			spansBuilder.add(Collections.singleton("tagmark"), matcher.end(GROUP_OPEN_BRACKET) - matcher.start(GROUP_OPEN_BRACKET));
+        			spansBuilder.add(Collections.singleton("anytag"), matcher.end(GROUP_ELEMENT_NAME) - matcher.end(GROUP_OPEN_BRACKET));
+
+        			if(!attributesText.isEmpty()) {
+        				
+        				lastKwEnd = 0;
+        				
+        				Matcher amatcher = ATTRIBUTES.matcher(attributesText);
+        				while(amatcher.find()) {
+        					spansBuilder.add(Collections.emptyList(), amatcher.start() - lastKwEnd);
+        					spansBuilder.add(Collections.singleton("attribute"), amatcher.end(GROUP_ATTRIBUTE_NAME) - amatcher.start(GROUP_ATTRIBUTE_NAME));
+        					spansBuilder.add(Collections.singleton("tagmark"), amatcher.end(GROUP_EQUAL_SYMBOL) - amatcher.end(GROUP_ATTRIBUTE_NAME));
+        					spansBuilder.add(Collections.singleton("avalue"), amatcher.end(GROUP_ATTRIBUTE_VALUE) - amatcher.end(GROUP_EQUAL_SYMBOL));
+        					lastKwEnd = amatcher.end();
+        				}
+        				if(attributesText.length() > lastKwEnd)
+        					spansBuilder.add(Collections.emptyList(), attributesText.length() - lastKwEnd);
+        			}
+
+        			lastKwEnd = matcher.end(GROUP_ATTRIBUTES_SECTION);
+        			
+        			spansBuilder.add(Collections.singleton("tagmark"), matcher.end(GROUP_CLOSE_BRACKET) - lastKwEnd);
+        		}
+        	}
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+ 
     @FXML
     private void acceptAction(ActionEvent event) {
         String filePath = filePathAndNameTextFieldValue.getText();
-        String content = xmlTextArea.getText();
+        String content = codeArea.getText();
 
         if (filePath == null || filePath.isBlank()) {
             showAlert(AlertType.ERROR, "No file selected", "Please select an XML file first.");
@@ -169,8 +238,10 @@ public class XmlEditorViewController implements Initializable, IViewController {
     @Override
     public void initialize(URL location, ResourceBundle rb) {
         this.rb = rb;
-
-        xmlTextArea.textProperty().addListener((ObservableValue<? extends String> observable, String oldText, String newText) -> {
+        acceptButton.setDisable(true);
+        
+        codeArea.textProperty().addListener((ObservableValue<? extends String> observable, String oldText, String newText) -> {
+            codeArea.setStyleSpans(0, computeHighlighting(newText));
             if(!oldText.isEmpty() && !newText.equals(originalText)) {
                 acceptButton.setDisable(false);
             } else {
